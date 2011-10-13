3e7c363d0c3d67fe3213bcf14c24fb9916de00e6
Michal <wtk300@gmail.com>
2011-10-13 21:17:57 +0200

    dodanie conts 2

---
+++ src/net/andruszko/lepapp/BundleConstants.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.andruszko.lepapp;
+
+/**
+ *
+ * @author wtk300
+ */
+public class BundleConstants {
+    
+    
+    public static final String SESSION_ID = "sessionId";
+    public static final String LEP_TYPE= "lepType";   
+    public static final String SUBSECTION_ID = "subSectionId";
+    public static final String LANG_ID = "langId";
+    
+    private  BundleConstants(){}
+    
+    
+    
+}
---
+++ src/net/andruszko/lepapp/vo/SpinnerDict.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.andruszko.lepapp.vo;
+
+/**
+ *
+ * @author wtk300
+ */
+public class SpinnerDict {
+    
+    private Integer id;
+    
+    private String descrition;
+
+    public SpinnerDict(Integer id, String descrition) {
+        this.id = id;
+        this.descrition = descrition;
+    }
+    
+    public String getDescrition() {
+        return descrition;
+    }
+
+    public void setDescrition(String descrition) {
+        this.descrition = descrition;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+}
