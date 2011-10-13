a494cdfad382048290e44bc80a40fb83b2d7b5bb
Michal <wtk300@gmail.com>
2011-10-13 21:27:26 +0200

    branch test cont

---
+++ src/net/andruszko/lepapp/vo/ItemTest.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.andruszko.lepapp.vo;
+
+/**
+ *
+ * @author wtk300
+ */
+public class ItemTest {
+    
+}
